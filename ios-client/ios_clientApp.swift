f3575b2ce25b7aa096ee64c3d943b07f84a4e8b3
Iury Souza <iury.souza@ifood.com.br>
2021-06-28 11:53:19 -0300

    Initial Commit

---
+++ ios-client/ios_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_clientApp.swift
+//  ios-client
+//
+//  Created by Iury on 28/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ios_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
